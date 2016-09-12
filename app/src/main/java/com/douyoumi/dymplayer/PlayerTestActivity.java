46bae4287dd36ef4858de3bccabcc897a6451b49
chenbo <chenbo@youku.com>
2016-09-12 12:48:20 +0800

    Initial commit

---
+++ app/src/main/java/com/douyoumi/dymplayer/PlayerTestActivity.java
@@ -0,0 +1,13 @@
+package com.douyoumi.dymplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_test);
+    }
+}
